35bf13c7587a43b5a62df3e8694480fa18f2b939
onyou.moon <onyou0031@naver.com>
2019-12-11 18:04:17 +0900

    그냥 보기좋게 바꿔줌 다음에 알아먹기 쉬울거야.. 더이상의 클린코드는 의미 없을듯

--- app/src/main/java/com/example/snslogin/MainActivity.kt
+++ app/src/main/java/com/example/snslogin/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.content.Intent
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.*
 import com.facebook.login.LoginManager
@@ -22,15 +21,18 @@ import com.twitter.sdk.android.core.TwitterCore
 import com.linecorp.linesdk.auth.LineLoginApi
 import com.linecorp.linesdk.LineApiResponseCode
 
-//import sun.jvm.hotspot.utilities.IntArray // ㅇㅐ는 왜계속 에러가 날까.. 구글링 해보자 ...
+//import sun.jvm.hotspot.utilities.IntArray                                                         // TODO ㅇㅐ는 왜계속 에러가 날까.. 구글링 해보자 ...
+
+/*
+* 사용자 로그인 정보 받아올때 각각의 프로필 사진과 각각의 이메일은
+* 반드시 null 처리 해주세요
+* */
 
 class MainActivity : AppCompatActivity() , View.OnClickListener{
     
     private var facebookManager: CallbackManager? = null
     private val twitterCoreSession = TwitterCore.getInstance().sessionManager.activeSession
     private val twitterAuthClient = TwitterAuthClient()
-    private var twitterInfo1 = ""
-    private var facebookInfo1 = ""
     private val lineChannelId = "1653637332"
     private val lineRequestCode = 2000
 
@@ -77,17 +79,24 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
 
                 //Profile.getCurrentProfile() 여기 자체에서도 기본적인거는 많이 가져올수 잇는데 ...
 
-                val request:GraphRequest
+                val request:GraphRequest = GraphRequest.newMeRequest(result.accessToken) { user, response ->
 
-                request = GraphRequest.newMeRequest(result.accessToken) { user, response ->
 
                     if (user.has("email")) {
-                        Log.e("facebook email", user.getString("email")) // 이메일은 예외처리 해줘야겟다 ,,
+                        val email= user.getString("email")
+                        Log.v("OnYou","facebook email : $email")
                     }
 
-                    facebookInfo1 = "user_id : ${user.getString("id")} \n user_name : ${user.getString("name")} \n AccessToken : ${result.accessToken.token}"
+                    val userId = user.getString("id")                                 // 숫자로 된 아이 입니다!
+                    val userName = user.getString("name")                             // 한글이름 입니다!
+                    val accessToken = result.accessToken.token                              // 엄청 길어요!
+                    val imageUrl = Profile.getCurrentProfile().getProfilePictureUri(200,200)
+
+                    var facebookInfo1 = "user_id:$userId\nuser_name:$userName\nAccessToken:$accessToken"
                     main_text_user_info.text = facebookInfo1
-                    Picasso.get().load(Profile.getCurrentProfile().getProfilePictureUri(200,200)).into(main_image_profile)
+
+
+                    Picasso.get().load(imageUrl).into(main_image_profile)
 
                 }
                 val parameters = Bundle()
@@ -97,7 +106,7 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
             }
 
             override fun onError(error:FacebookException) {
-                Log.w("onYou", "Error: ${error.message}")
+                Log.v("onYou", "Error: ${error.message}")
             }
 
             override fun onCancel() {
@@ -107,16 +116,15 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
     }
 
     private fun loginTwitter() {
-        if (twitterCoreSession == null){
+        if (twitterCoreSession == null){                                                            //이 아이 덕분에 세션이 있으면 그냥 바로 불러올수 있습니다
             twitterAuthClient.authorize(this, object : Callback<TwitterSession>() {
-                override fun success(result: Result<TwitterSession>) {                              // 세션이 아직 없다면
-                    Toast.makeText(this@MainActivity, "Success", Toast.LENGTH_SHORT).show()
+                override fun success(result: Result<TwitterSession>) {
                     val twitterSession = result.data
                     fetchTwitterEmail(twitterSession)
                 }
 
                 override fun failure(exception: TwitterException) {
-                    Toast.makeText(this@MainActivity, "Failure", Toast.LENGTH_SHORT).show()
+                    Log.v("OnYou", "twitterAuthClient.authorize Failure ${exception.message}")
                 }
             })
         } else {
@@ -128,20 +136,20 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
         twitterAuthClient.requestEmail(twitterSession, object : Callback<String>() {
             @SuppressLint("SetTextI18n")
             override fun success(result: Result<String>) {
-                Toast.makeText(this@MainActivity, "fetchTwitterEmail", Toast.LENGTH_SHORT).show()
 
-                val authToken = twitterSession?.authToken
+                val authToken = twitterSession?.authToken                          // authToken 도 fetchTwitterEmail 에서만 가져올 수 있습니다!
                 val token = authToken?.token
                 val secret = authToken?.secret
-
-                twitterInfo1 = "token : \n $token \n\n secret : $secret \n\n"
-                val twitterInfo2 = "e-mail : ${result.data} \n\n user_id : ${twitterSession?.userId} \n\n screen_name :  ${twitterSession?.userName}" // 유져네임은 시크릿하게만 가져 온다,아래꺼랑 다르게 토큰 가져 올수 있음 온유라는 이름은 못가져옴 // 유저 아이디 (숫자)/ 이메일은 밑에랑 동알하게 가져옴
+                val email = result.data
+                val uerId = twitterSession?.userId                                           // 숫자만 나오는 아이디 입니다!
+                val screenName = twitterSession?.userName                                   // 영어와 숫자가 섞여 있는애 입니다! fetchTwitterEmail 에서만 가져올수 있습니다 !
+                val twitterInfo = "e-mail:$email\n\n user_id:$uerId\n\nscreen_name:$screenName"
+                main_text_user_info.text = twitterInfo
                 fetchTwitterProfileUrl()
-                main_text_user_info.text = "$twitterInfo1  $twitterInfo2"
             }
 
             override fun failure(exception: TwitterException) {
-                Toast.makeText(this@MainActivity, "뭔가 잘못됬어요.", Toast.LENGTH_SHORT).show()
+                Log.v("OnYou", "requestEmail Failure ${exception.message}")
             }
         })
     }
@@ -151,21 +159,19 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
         user.enqueue(object : Callback<User>() {
 
             override fun success(userResult: Result<User>) {
-                val name = userResult.data.name // 유저네임은 정확하게 온유라고 가져옴 ..
-                val email = userResult.data.email //여기서도 이메일 잘가져온다......
-              //  main_text_user_info_twitter.text = "$name  $email"
 
-                val photoUrlNormalSize = userResult.data.profileImageUrlHttps // 이미지 뜬듯.. 힘들다..
-//                val photoUrlBiggerSize = userResult.data.profileImageUrl.replace("_normal", "_bigger")
-//                val photoUrlMiniSize = userResult.data.profileImageUrl.replace("_normal", "_mini")
-//                val photoUrlOriginalSize = userResult.data.profileImageUrl.replace("_normal", "")
+                val name = userResult.data.name                                             // 한글로 정확하게 이름(ex.온유)은 여기서 가져올수 있습니다 fetchTwitterEmail() 에서는 안되더라구요 .. 못찾는 건가
+                val photoUrlNormalSize = userResult.data.profileImageUrlHttps
+                //val photoUrlBiggerSize = userResult.data.profileImageUrl.replace("_normal", "_bigger")
+                //val photoUrlMiniSize = userResult.data.profileImageUrl.replace("_normal", "_mini")
+                //val photoUrlOriginalSize = userResult.data.profileImageUrl.replace("_normal", "") // 나중에 사진 사이즈를 다르게 가져오고 싶을때 사용하세요!
 
                 Picasso.get().load(photoUrlNormalSize).into(main_image_profile)
 
             }
 
             override fun failure(exc: TwitterException) {
-                Log.d("TwitterKit", "Verify Credentials Failure", exc)
+                Log.v("OnYou", "Verify Credentials Failure ${exc.message}")
             }
         })
     }
@@ -174,39 +180,38 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
     override fun onActivityResult(requestCode: Int, responseCode: Int, intent: Intent?) {
         twitterAuthClient.onActivityResult(requestCode, responseCode, intent)
         facebookManager?.onActivityResult(requestCode, responseCode, intent)
-        if (requestCode == lineRequestCode) lineRequest(requestCode,intent)
+        if (requestCode == lineRequestCode) lineRequest(intent)
 
     }
 
     @SuppressLint("SetTextI18n")
-    private fun lineRequest(requestCode: Int, intent: Intent?){
+    private fun lineRequest( intent: Intent?){
+
         val result = LineLoginApi.getLoginResultFromIntent(intent)
 
         when (result.responseCode) {
 
-            LineApiResponseCode.SUCCESS -> {
-                // Login successful
+            LineApiResponseCode.SUCCESS -> {                                                        // Login successful
 
                 val email = result.lineIdToken?.email
 
                 val accessToken = result.lineCredential!!.accessToken.tokenString
-                val displayName = result.lineProfile!!.displayName
+                val displayName = result.lineProfile!!.displayName                           // 한글이름(ex.온유) 입니다!
                 val statusMessage = result.lineProfile!!.statusMessage
-                val userId = result.lineProfile!!.userId
-                val profileUrl = result.lineProfile!!.pictureUrl.toString()
+                val userId = result.lineProfile!!.userId                                     // 숫자와 영어만 나오는 아이디 입니다!!
+                val profileUrl = result.lineProfile!!.pictureUrl.toString()                  // 사용자가 사진을 설정 안했으면 null 로 들어옵니다(그냥 아무 그림도 안뜸), 트위터는 기본이미지가 들어오더라구요!
                 main_text_user_info.text = "email: $email\n displayName : $displayName \n statusMessage: $statusMessage \n userId : $userId \n accessToken: $accessToken"
-                Picasso.get().load(profileUrl).into(main_image_profile) // 사용자가 사진을 설정 안했으면 null 로 들어온다(그냥 아무 그림도 안뜸 ), 트위터는 기본이미지가 들어옴
-
+                Picasso.get().load(profileUrl).into(main_image_profile)
             }
 
-            LineApiResponseCode.CANCEL ->
-                // Login canceled by user
-                Log.e("ERROR", "LINE Login Canceled by user.")
+            LineApiResponseCode.CANCEL ->                                                           // Login canceled by user
 
-            else -> {
-                // Login canceled due to other error
-                Log.e("ERROR", "Login FAILED!")
-                Log.e("ERROR", result.errorData.toString())
+                Log.v("OnYou", "LINE Login Canceled by user.")
+
+            else -> {                                                                               // Login canceled due to other error
+
+                Log.v("OnYou", "Login FAILED!")
+                Log.v("OnYou", result.errorData.toString())
             }
         }
 
